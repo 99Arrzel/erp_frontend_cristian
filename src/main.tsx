588354d9e4fe2f682d69a74bb3bba424e3c01cff
Andrés Carrillo <af.carrillo@live.com>
2023-03-28 14:43:24 -0400

    Baseurl

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-export const baseUrl = 'http://localhost:3333';
+export const baseUrl = 'http://134.195.91.224:3333';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
