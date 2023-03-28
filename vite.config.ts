ad9c4efbe3f5fc23a561d72e3af8d83d63535c3d
Andrés Carrillo <af.carrillo@live.com>
2023-03-28 14:46:27 -0400

    host master

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 
-
+  server: {
+    port: 4173,
+    host: true
+  },
   plugins: [react()],
 
 });
