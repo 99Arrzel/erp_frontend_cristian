2c636ac446781d56d9beb452d1cdb34bdc808e8d
Andrés Carrillo <af.carrillo@live.com>
2023-04-19 16:42:17 -0400

    baseurl

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import './index.css';
 //export const baseUrl = 'https://backend.pruebita.tk';
 //export const baseUrlReports = 'https://reportes.pruebita.tk';
 
-export const baseUrl = 'http://localhost:3333';
-export const baseUrlReports = 'https://reportes.pruebita.tk';
+export const baseUrl = 'http://arrzel.com:3333';
+export const baseUrlReports = 'https://reportes.arrzel.com';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
