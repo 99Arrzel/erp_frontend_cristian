ee672986b05215017e79d0012cd820c47722334a
Andres Carrillo <af.carrillo@live.com>
2023-05-20 15:49:14 -0400

    Env var

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-export const baseUrl = 'https://escobarbkn.arrzel.com';
+export const baseUrl = import.meta.env.VITE_BASE_URL as string;
 //export const baseUrl = 'http://localhost:3333';
-export const baseUrlReports = 'https://reportes.arrzel.com';
-
-
-
+export const baseUrlReports = import.meta.env.VITE_BASE_URL_REPORTS as string;
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/* export default defineConfig({
 
   server: {
     port: 4173,
@@ -10,4 +10,20 @@ export default defineConfig({
   },
   plugins: [react()],
 
-});
+}); */
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    define: {
+      API_URL: JSON.stringify(process.env.VITE_BASE_URL),
+      REPORTES_URL: JSON.stringify(process.env.VITE_BASE_URL_REPORTS),
+    },
+    server: {
+      port: 4173,
+      host: true
+    },
+    plugins: [react()],
+  });
+};
